828c9236f0429e5ce686b19624a0bc0268bd58f3
Rainier Sapin <rainiersapin0131@gmail.com>
2023-09-18 19:07:46 +0800

    Add Home Design (Dashboard and Header)

--- static/login.js
+++ static/login.js
@@ -4,11 +4,14 @@ const password = document.getElementById("password");
 const usernameHelper = document.querySelector(".login-username-helper");
 const passwordHelper = document.querySelector(".login-password-helper");
 
-
+if (usernameHelper){
 username.addEventListener("input", ()=>{
     usernameHelper.style.display = "none";
 });
+}
 
+if (passwordHelper){
 password.addEventListener("input", ()=>{
     passwordHelper.style.display = "none";
 });
+}
--- static/signup.js
+++ static/signup.js
@@ -13,23 +13,32 @@ const passwordHelper = document.querySelector(".signup-password-helper");
 const confirm_password = document.getElementById("confirm-password-signup");
 const confirm_passwordHelper = document.querySelector(".signup-confirm_password-helper");
 
+if (fnameHelper){
 fname.addEventListener("input", ()=>{
     fnameHelper.style.display = "none";
 });
+}
 
+if (lnameHelper){
 lname.addEventListener("input", ()=>{
     lnameHelper.style.display = "none";
 });
+}
 
+if (usernameHelper){
 username.addEventListener("input", ()=>{
     usernameHelper.style.display = "none";
 });
+}
 
+if (passwordHelper){
 password.addEventListener("input", ()=>{
     passwordHelper.style.display = "none";
 });
+}
 
+if (confirm_passwordHelper){
 confirm_password.addEventListener("input", ()=>{
     confirm_passwordHelper.style.display = "none";
 });
-
+}
